5279ffbf4f88d787331134a3fec4d97e8b2038b4
marc <70189311+Marc2204@users.noreply.github.com>
2025-02-20 00:32:13 +0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -2,12 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("helpModal");
     const btn = document.getElementById("helpButton");
     const closeBtn = document.querySelector(".close");
-    const skipBtn = document.getElementById("skipButton");
 
-    // Check if the user has skipped the instructions before
-    if (!localStorage.getItem("modalSkipped")) {
-        modal.style.display = "flex";
-    }
+
+     modal.style.display = "flex";
+
 
     // Open Modal
     btn.addEventListener("click", function () {
@@ -17,12 +15,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Close Modal
     closeBtn.addEventListener("click", function () {
         modal.style.display = "none";
-    });
-
-    // Skip Button (Close modal & prevent future appearance)
-    skipBtn.addEventListener("click", function () {
-        modal.style.display = "none";
-        localStorage.setItem("modalSkipped", "true");
     });
 
     // Close when clicking outside the modal
